76bd7c5d8fe21d02a93ab65b5582a5042ae0931c
Serkan TOY <stoy@windowslive.com>
2025-01-30 10:00:50 +0300

    Header degistirildi - login sayfasi yapilandirildi

--- Pages/Account/Login.cshtml.cs
+++ Pages/Account/Login.cshtml.cs
@@ -1,78 +1,1 @@
-using eticaret.Domain;
-using eticaret.Domain.Entities.Users;
-using eticaret.Models.Login;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
-using System.Security.Claims;
-
-namespace eticaret.Pages.Account
-{
-    public class LoginModel : PageModel
-    {
-        private UserManager<User> _userManager;
-        private SignInManager<User> _signInManager;
-        public LoginModel(UserManager<User> _userManager, SignInManager<User> _signInManager)
-        {
-            this._userManager = _userManager;
-            this._signInManager = _signInManager;
-        }
-
-
-        [BindProperty]
-        public LoginViewModel loginViewModel { get; set; }
-        public void OnGet()
-        {
-        }
-
-        public async Task OnPostLoginData(string? ReturnUrl = null)
-        {
-            //ReturnUrl = ReturnUrl ?? Url.Action("Index", "Home", new { area = "" });
-            if (ModelState.IsValid)
-            {
-                User hasUser = await _userManager.FindByEmailAsync(loginViewModel.Email);
-
-                if (hasUser == null)
-                {
-                    ModelState.AddModelError($"{loginViewModel.Email}", "Kullanýcý  Bulunmamaktadýr!");
-                }
-
-                if (!hasUser.IsLocked && !hasUser.IsDeleted)
-                {
-                    Microsoft.AspNetCore.Identity.SignInResult result = await _signInManager.PasswordSignInAsync(user: hasUser, password: loginViewModel.Password, isPersistent: loginViewModel.RememberMe, lockoutOnFailure: true);
-
-                    if (result.IsLockedOut)
-                    {
-                        ModelState.AddModelError("", "3 Hatalý Giriþ'ten Sonra 5 Dakika Süre ile Hesabiniz Kilitlenmiþtir. Lütfen 5 Dakika Bekleyin.");                        
-                    }
-
-                    if (result.Succeeded)
-                    {
-                        await _userManager.ResetAccessFailedCountAsync(hasUser);
-                        var claims = new List<Claim>
-                        {
-                            new Claim(ClaimTypes.Email, hasUser!.Email!),
-                            new Claim("Id", hasUser.Id.ToString()),
-                            new Claim("FullName", $"{hasUser.Name.CustomToTitleCase()} {hasUser.SurName.CustomToUpper()}"),
-                            new Claim("PhoneNumber", $"{hasUser.PhoneNumber}"),
-                            new Claim("Flag", $"{hasUser.Flag}"),
-                            new Claim("EPosta", $"{hasUser.Email}")
-                        };
-                        await _signInManager.SignInWithClaimsAsync(hasUser, false, claims);
-                        //return RedirectToAction("Index", "Home", new { area = "" });
-                    }
-                    else
-                    {
-                        ModelState.AddModelError("", "Giriþ Ýþlemi Baþarýsýz.");
-                    }
-                }
-                else
-                {
-                    ModelState.AddModelError($"{loginViewModel.Email}", "Giriþ Ýþlemi Baþarýsýz.");                 
-                    ModelState.AddModelError($"{loginViewModel.Password}", "Giriþ Ýþlemi Baþarýsýz.");                 
-                }
-
-            }
-        }
-    }
-}
+ï»¿using eticaret.Domain;using eticaret.Domain.Entities.Users;using eticaret.Models.Login;using Microsoft.AspNetCore.Components;using Microsoft.AspNetCore.Identity;using Microsoft.AspNetCore.Mvc;using Microsoft.AspNetCore.Mvc.RazorPages;using Microsoft.EntityFrameworkCore.Metadata.Internal;using System.Security.Claims;namespace eticaret.Pages.Account{	public class LoginModel : PageModel	{		private UserManager<User> _userManager;		private SignInManager<User> _signInManager;		public LoginModel(UserManager<User> _userManager, SignInManager<User> _signInManager)		{			this._userManager = _userManager;			this._signInManager = _signInManager;		}		[BindProperty]		public LoginViewModel loginViewModel { get; set; }		public void OnGet()		{		}		public async Task OnPostLoginData(string? ReturnUrl = null)		{			//ReturnUrl = ReturnUrl ?? Url.Action("Index", "Home", new { area = "" });			if (ModelState.IsValid)			{				User hasUser = await _userManager.FindByEmailAsync(loginViewModel.Email);				if (hasUser == null)				{					ModelState.AddModelError($"{loginViewModel.Email}", "KullanÄ±cÄ±  BulunmamaktadÄ±r!");				}								if (!hasUser!.IsLocked && !hasUser.IsDeleted)				{					Microsoft.AspNetCore.Identity.SignInResult result = await _signInManager.PasswordSignInAsync(user: hasUser, password: loginViewModel.Password, isPersistent: loginViewModel.RememberMe, lockoutOnFailure: true);					if (result.IsLockedOut)					{						ModelState.AddModelError("", "3 HatalÄ± GiriÅŸ'ten Sonra 5 Dakika SÃ¼re ile Hesabiniz KilitlenmiÅŸtir. LÃ¼tfen 5 Dakika Bekleyin.");                    }					if (result.Succeeded)					{						await _userManager.ResetAccessFailedCountAsync(hasUser);						var claims = new List<Claim>						{							new Claim(ClaimTypes.Email, hasUser!.Email!),							new Claim("Id", hasUser.Id.ToString()),							new Claim("FullName", $"{hasUser.Name.CustomToTitleCase()} {hasUser.SurName.CustomToUpper()}"),							new Claim("PhoneNumber", $"{hasUser.PhoneNumber}"),							new Claim("Flag", $"{hasUser.Flag}"),							new Claim("EPosta", $"{hasUser.Email.CustomToLower()}")						};						await _signInManager.SignInWithClaimsAsync(hasUser, false, claims);                        var context = new Microsoft.AspNetCore.Http.HttpContextAccessor();						context.HttpContext.Response.Redirect("/", true);                        //return RedirectToPage("");                    }					else					{						ModelState.AddModelError("", "GiriÅŸ Ä°ÅŸlemi BaÅŸarÄ±sÄ±z.");                    }				}				else				{					ModelState.AddModelError($"{loginViewModel.Email}", "GiriÅŸ Ä°ÅŸlemi BaÅŸarÄ±sÄ±z.");					ModelState.AddModelError($"{loginViewModel.Password}", "GiriÅŸ Ä°ÅŸlemi BaÅŸarÄ±sÄ±z.");                }			}		}	}}
