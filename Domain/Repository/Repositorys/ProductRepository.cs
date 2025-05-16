ffc8b6656ba3cd4d2729aa7e7974ce02a752ca33
Serkan TOY <stoy@windowslive.com>
2025-05-16 10:50:54 +0300

    Anasayfa yeni ürünler alanı yapılandırıldı

--- Domain/Repository/Repositorys/ProductRepository.cs
+++ Domain/Repository/Repositorys/ProductRepository.cs
@@ -3,8 +3,10 @@ using eticaret.Domain.Database.Context;
 using eticaret.Domain.Repository.Interface;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
+using System.Globalization;
 using System.Linq;
 using System.Linq.Expressions;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace eticaret.Domain.Repository.Repositorys
 {
@@ -64,7 +66,65 @@ namespace eticaret.Domain.Repository.Repositorys
                             Rating = v.Ratin.Rating
                         }
                     }).Max(f => f.Ratin.Rating) : 0,
+                    Flag = x.Flag,
+                    CreateDate = x.CreateDate,
                 }).ToList();
+        }
+
+
+        public List<Product> ProductJoinTake(Expression<Func<Product, bool>> predicate = null)
+        {
+            return context.Product.Where(predicate).
+                Include(j => j.SubCategory).ThenInclude(g => g.Category).
+                Include(j => j.ColorProduct).ThenInclude(g => g.Colors).
+                Include(i => i.Images).
+                Include(r => r.RatinProducts).ThenInclude(g => g.Ratin).Select(x => new Product
+                {
+                    Id = x.Id,
+                    Name = x.Name,
+                    IsDeleted = x.IsDeleted,
+                    SubCategory = x.SubCategory,
+                    Category = x.SubCategory.Category,
+                    ColorProduct = x.ColorProduct,
+                    Colors = x.ColorProduct.Select(c => new Colors
+                    {
+                        Id = c.Colors.Id,
+                        Colorvalue = c.Colors.Colorvalue,
+                    }).ToList(),
+                    RatinProducts = x.RatinProducts.Select(v => new RatinProduct
+                    {
+                        Id = v.Id,
+                        ProductId = v.ProductId,
+                        RatinId = v.RatinId,
+                        Ratin = new Ratin
+                        {
+                            Id = v.Ratin.Id,
+                            Rating = v.Ratin.Rating
+                        }
+                    }).ToList(),
+                    NewPrice = x.NewPrice,
+                    OldPrice = x.OldPrice,
+                    Description = x.Description,
+                    Images = x.Images.Where(x => x.IsDeleted == false).Select(i => new Image
+                    {
+                        Id = i.Id,
+                        Name = i.Name,
+                        IsShowcase = i.IsShowcase
+                    }).ToList(),
+                    RatinMax = x.RatinProducts.Count() != 0 ? x.RatinProducts.Select(v => new RatinProduct
+                    {
+                        Id = v.Id,
+                        ProductId = v.ProductId,
+                        RatinId = v.RatinId,
+                        Ratin = new Ratin
+                        {
+                            Id = v.Ratin.Id,
+                            Rating = v.Ratin.Rating
+                        }
+                    }).Max(f => f.Ratin.Rating) : 0,
+                    Flag = x.Flag,
+                    CreateDate = x.CreateDate,
+                }).OrderByDescending(f => f.CreateDate).Take(8).ToList();
         }
 
 
