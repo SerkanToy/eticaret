10daea89a0acec60c3d7efb97f2f32bb457ff7d4
Serkan TOY <stoy@windowslive.com>
2025-07-17 09:04:12 +0300

    Razor Component Limit artışı yapıldı ve Ürünler tablosu favoriye etleme yapılandırıl

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 ﻿using eticaret.Domain.Database.Context;
 using eticaret.Domain.Extensions;
 using eticaret.Models.MailMessageSend;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.Extensions.DependencyInjection;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,13 +12,21 @@ builder.Services.AddRazorPages();
 builder.Services.Configure<EmailConfiguration>(builder.Configuration.GetSection("EmailConfiguration"));
 
 builder.Services.AddDbContext<CommerceContext>();
-builder.Services.AddServerSideBlazor().AddInteractiveServerComponents();
+builder.Services.AddServerSideBlazor()
+    .AddInteractiveServerComponents();
 
 builder.Services.IdentityAdd();
 builder.Services.PolicyAdd();
 
 builder.Services.TransientAdd();
 builder.Services.ScopedAdd();
+
+builder.Services.Configure<HubOptions>(options =>
+{
+    //options.MaximumReceiveMessageSize = 2048 * 2048;
+    options.MaximumReceiveMessageSize = null; // no limit or use a number
+});
+
 
 var app = builder.Build();
 
@@ -27,6 +37,7 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
 
 app.UseHttpsRedirection();
 
