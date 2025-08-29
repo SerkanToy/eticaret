3c24a8e992c7df995783e02f0a86e2a850bbe34d
Serkan TOY <stoy@windowslive.com>
2025-08-29 10:40:03 +0300

    Sipariş sayfası yapılandırıldı ve Mobil uyumluluk yapılan arayüz hataları giderildi

---
+++ Pages/Myorders/MyOrders.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace eticaret.Pages.Myorders
+{
+    public class MyOrdersModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
